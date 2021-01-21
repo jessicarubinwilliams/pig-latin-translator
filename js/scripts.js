66d6b4257bfa31872ccb6318a6f664d36cc9cdd2
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-01-20 20:25:40 -0800

    Refactor TDD test1 to add a break in the loop.
    Co-authored-by: Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
+// let isAVowel = false; 
+// const vowelsArray = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+// vowelsArray.forEach(function(vowel) {
+//   if (vowel === "e") {
+//     isAVowel = true;
+//   };
+//   console.log("Looped!")
+// });
+// isAVowel
+
 let isAVowel = false; 
-const vowelsArray = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
-vowelsArray.forEach(function(vowel) {
-  if (vowel === "e") {
-    isAVowel = true;
-  };
-  console.log("Looped!")
-});
+  const vowelsArray = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  for (let index = 0; index < vowelsArray.length; index +=1) {
+    if (vowelsArray[index] === "e") {
+      isAVowel = true;
+      break;
+    };
+    console.log("Looped!");
+  }
 isAVowel
