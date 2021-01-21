bacb2181ea562adb4d013422b733a9730d822f9f
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-01-20 20:37:58 -0800

    Refactor TDD test1 to compare first character of userInput to vowelsArray.
    Co-authored-by: Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,22 @@
 //   }
 // isAVowel
 
+// function vowelDetector(vowelsArray, firstCharacter) {
+//   for (let index = 0; index < vowelsArray.length; index +=1) {
+//     if (vowelsArray[index] === firstCharacter) {
+//       return true;
+//     };
+//     console.log("Looped!");
+//   }
+//   return false;
+// }
+
+// const vowelsArray = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+// const firstCharacter1 = "e";
+// const firstCharacter2 = "b";
+// vowelDetector(vowelsArray, firstCharacter1);
+// vowelDetector(vowelsArray, firstCharacter2);
+
 function vowelDetector(vowelsArray, firstCharacter) {
   for (let index = 0; index < vowelsArray.length; index +=1) {
     if (vowelsArray[index] === firstCharacter) {
@@ -29,8 +45,7 @@ function vowelDetector(vowelsArray, firstCharacter) {
   return false;
 }
 
+let userInput = "Ice"
 const vowelsArray = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
-const firstCharacter1 = "e";
-const firstCharacter2 = "b";
-vowelDetector(vowelsArray, firstCharacter1);
-vowelDetector(vowelsArray, firstCharacter2);
+const firstCharacter = userInput.charAt(0);
+vowelDetector(vowelsArray, firstCharacter);
