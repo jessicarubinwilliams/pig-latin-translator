ed9be6ebd6af3091f8e55e566e40b59c67abc4b1
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-01-20 20:30:32 -0800

    Refactor TDD test to declare a function.
    Co-authored-by: Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,29 @@
 // });
 // isAVowel
 
-let isAVowel = false; 
-  const vowelsArray = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+// let isAVowel = false; 
+//   const vowelsArray = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+//   for (let index = 0; index < vowelsArray.length; index +=1) {
+//     if (vowelsArray[index] === "e") {
+//       isAVowel = true;
+//       break;
+//     };
+//     console.log("Looped!");
+//   }
+// isAVowel
+
+function vowelDetector(vowelsArray, firstCharacter) {
   for (let index = 0; index < vowelsArray.length; index +=1) {
-    if (vowelsArray[index] === "e") {
-      isAVowel = true;
-      break;
+    if (vowelsArray[index] === firstCharacter) {
+      return true;
     };
     console.log("Looped!");
   }
-isAVowel
+  return false;
+}
+
+const vowelsArray = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+const firstCharacter1 = "e";
+const firstCharacter2 = "b";
+vowelDetector(vowelsArray, firstCharacter1);
+vowelDetector(vowelsArray, firstCharacter2);
